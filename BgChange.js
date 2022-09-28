70c0da895f17bef74642149486c931181f1e40a9
D.Raposo <decioraposopro@gmil.com>
2022-09-28 18:29:33 -0300

    ADD:bg change function

---
+++ BgChange.js
@@ -0,0 +1,7 @@
+var section = document.querySelector('section');
+var images = [];
+
+setInterval(function () {
+    var bg = images[Math.floor(Math.random() * images.length)];
+    section.style.background = bg;
+}, 1000);
