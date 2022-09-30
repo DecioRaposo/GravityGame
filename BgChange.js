69a8ec7378146c539c7bcf67f5bc9ba24da3cb1c
D.Raposo <decioraposopro@gmil.com>
2022-09-30 11:19:38 -0300

    ADD:Bg change colors

--- BgChange.js
+++ BgChange.js
@@ -1,9 +1,19 @@
-var section = document.querySelector('section');
-var images = [
-
-];
-
+function generateColor() {
+    length = 6;
+    var chars = 'abcdef0123456789';
+    var color = '';
+    for (var i = 0; i < length; i++) {
+        color += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return '#' + color;
+}
+function changeColor() {
+    document.body.style.backgroundColor = generateColor();
+    document.body.style.backgroundColor = generateColor();
+    generateColor();
+}
+//Change background every 1 second
 setInterval(function () {
-    var bg = images[Math.floor(Math.random() * images.length)];
-    section.style.background = bg;
+    changeColor();
+
 }, 1000);
